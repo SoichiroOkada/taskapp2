2063f6d8e9e28dcee224eee137654b3227a98bb3
soichiro.okada <straycat0914@gmail.com>
2020-12-04 19:46:27 +0900

    searchBar(_:textDidChange:)，キャンセルボタン実装後

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -10,96 +10,144 @@ import UIKit
 import RealmSwift   // ←追加
 import UserNotifications    // 追加
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
-@IBOutlet weak var tableView: UITableView!
-@IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     // Realmインスタンスを取得する
     let realm = try! Realm()  // ←追加
     
-
-       
+    
+    
     // DB内のタスクが格納されるリスト。
     // 日付の近い順でソート：昇順
     // 以降内容をアップデートするとリスト内は自動的に更新される。
     var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)  // ←追加
-
+    
+    
+    //        // Query using an NSPredicate NSPredicateを使って検索条件を指定します
+    //        let predicate = NSPredicate(format: )
+    //        tanTasks = realm.objects(Task.self).filter(UISearchTextField)
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    tableView.delegate = self
-    tableView.dataSource = self
         
-        // Query using a predicate string 文字列で検索条件を指定します
-        var tanTasks = realm.objects(Task.self).filter("category = 'UISearchTextField.text'")
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        searchBar.placeholder = "カテゴリ検索"
+        searchBar.showsCancelButton = true
+        ////        // Query using a predicate string 文字列で検索条件を指定します
+        //        taskArray = realm.objects(Task.self).filter("category = searchBar.text")
+        //// Query using an NSPredicate
 
-//        // Query using an NSPredicate NSPredicateを使って検索条件を指定します
-//        let predicate = NSPredicate(format: )
-//        tanTasks = realm.objects(Task.self).filter(UISearchTextField)
-        
     }
     // データの数（＝セルの数）を返すメソッド
-      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return taskArray.count  // ←修正する
-      }
-
-      // 各セルの内容を返すメソッド
-      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-          // 再利用可能な cell を得る
-          let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count  // ←修正する
+    }
+    
+    // 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         // Cellに値を設定する.  --- ここから ---
         let task = taskArray[indexPath.row]
         cell.textLabel?.text = task.title
-
+        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
-
+        
         let dateString:String = formatter.string(from: task.date)
         cell.detailTextLabel?.text = dateString
         // --- ここまで追加 ---
+        
+        
+        return cell
+    }
+    internal func searchBar(_ searchBar: UISearchBar,
+                            textDidChange searchText: String) {
+        let predicate = NSPredicate(format: "category = %@", searchBar.text!)
+        taskArray = realm.objects(Task.self).filter(predicate)
+        tableView.reloadData()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
 
+    taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+    tableView.reloadData()
+    }
 
-          return cell
-      }
+//
+//    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+//     }
+//    func searchBar (_ searchBar: UISearchBar, textDidChange searchText: String) {
+//
+//        if searchBar.text == "" {          tableView.reloadData()
+//
+//    }
+//        else {
+//
+//        let predicate = NSPredicate(format: "category = %@", "searchBar.text")
+//        taskArray = realm.objects(Task.self).filter(predicate)
+//        tableView.reloadData()
+//    }
+//    }
+//    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+//    }
+//    //  検索バーに入力があったら呼ばれる
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        guard !searchText.isEmpty else {
+//            currentItems = items
+//            table.reloadData()
+//            return
+//        }
+//        taskArray = Task.filter({ item -> Bool in
+//            item.title.lowercased().contains(searchText.lowercased())
+//        })
+//        table.reloadData()
+//    }
 
-   
     
-      // 各セルを選択した時に実行されるメソッド
-      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         performSegue(withIdentifier: "cellSegue",sender: nil)
-      }
-
-      // セルが削除が可能なことを伝えるメソッド
-      func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
-          return .delete
-      }
+    
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+    
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+        return .delete
+    }
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         // --- ここから ---
-              if editingStyle == .delete {
-                  // 削除するタスクを取得する
-                  let task = self.taskArray[indexPath.row]
-
-                  // ローカル通知をキャンセルする
-                  let center = UNUserNotificationCenter.current()
-                  center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
-
-                  // データベースから削除する
-                  try! realm.write {
-                      self.realm.delete(task)
-                      tableView.deleteRows(at: [indexPath], with: .fade)
-                  }
-
-                  // 未通知のローカル通知一覧をログ出力
-                  center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
-                      for request in requests {
-                          print("/---------------")
-                          print(request)
-                          print("---------------/")
-                      }
-                  }
-              } // --- ここまで変更 ---
+        if editingStyle == .delete {
+            // 削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            // ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            // 未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("---------------/")
+                }
+            }
+        } // --- ここまで変更 ---
         
     }
     // 入力画面から戻ってきた時に TableView を更新させる
@@ -108,23 +156,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.reloadData()
     }
     // segue で画面遷移する時に呼ばれる
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-         let inputViewController:InputViewController = segue.destination as! InputViewController
-
-         if segue.identifier == "cellSegue" {
-             let indexPath = self.tableView.indexPathForSelectedRow
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
             inputViewController.task = taskArray[indexPath!.row]
             
-         } else {
-             let task = Task()
-
-             let allTasks = realm.objects(Task.self)
-             if allTasks.count != 0 {
-                 task.id = allTasks.max(ofProperty: "id")! + 1
-             }
-
-             inputViewController.task = task
-         }
-     }
+        } else {
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
 }
 
